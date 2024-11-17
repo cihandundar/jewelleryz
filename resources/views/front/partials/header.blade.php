6da156d68c9d35645c0f916e8970b44bd4c087c9
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-18 00:05:55 +0300

    Initial commit

---
+++ resources/views/front/partials/header.blade.php
@@ -0,0 +1,61 @@
+<div class="bg-blue-950 text-white py-2">
+    <div class="container max-w-screen-xl mx-auto">
+        <marquee>
+            <div class="flex gap-32">
+                <div class="flex items-center gap-2"><img class="w-5" src="{{ asset('front/assets/images/ring.png') }}"
+                        alt=""> Flat
+                    30 % Sale
+                    Going On
+                    Selected Products. <a class="uppercase" href="#">View
+                        Products.</a></div>
+                <div class="flex items-center gap-2"><img class="w-5"
+                        src="{{ asset('front/assets/images/ring.png') }}" alt=""> Flat
+                    30 % Sale
+                    Going On
+                    Selected Products. <a class="uppercase" href="#">View
+                        Products.</a></div>
+                <div class="flex items-center gap-2"><img class="w-5"
+                        src="{{ asset('front/assets/images/ring.png') }}" alt=""> Flat
+                    30 % Sale
+                    Going On
+                    Selected Products. <a class="uppercase" href="#">View
+                        Products.</a></div>
+                <div class="flex items-center gap-2"><img class="w-5"
+                        src="{{ asset('front/assets/images/ring.png') }}" alt=""> Flat
+                    30 % Sale
+                    Going On
+                    Selected Products. <a class="uppercase" href="#">View
+                        Products.</a></div>
+
+            </div>
+        </marquee>
+    </div>
+
+</div>
+<header class="py-3">
+    <div class="container max-w-screen-xl mx-auto">
+        <div class="row">
+            <div class="col-lg-4">
+                <nav>
+                    <ul class="flex gap-4">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Collections</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-lg-4">
+                <a href="/" class="flex justify-center">
+                    <img src="{{ asset('front/assets/images/logo.png') }}" alt="logo" class="w-40">
+                </a>
+            </div>
+            <div class="col-lg-4">
+                <ul class="flex justify-end gap-4 items-center">
+                    <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+                    <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
+                    <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
