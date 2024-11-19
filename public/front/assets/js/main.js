b9c31971ca0ff35f0161d0f8037b529f57f6148a
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-19 21:27:39 +0300

    slider'a efekt eklendi

--- public/front/assets/js/main.js
+++ public/front/assets/js/main.js
@@ -4,22 +4,22 @@ const swiper = new Swiper(".heroSlider", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-    // effect: "creative", // Creative efekti kullanıyoruz
-    // creativeEffect: {
-    //     prev: {
-    //         // Önceki slaytın kaybolma şekli
-    //         translate: ["-120%", 0, -500],
-    //         opacity: 0.5,
-    //     },
-    //     next: {
-    //         // Sonraki slaytın gelme şekli
-    //         translate: ["120%", 0, -500],
-    //         opacity: 0.5,
-    //     },
-    // },
-    // autoplay: {
-    //     delay: 3000, // Otomatik geçiş süresi (3 saniye)
-    //     disableOnInteraction: false, // Kullanıcı etkileşimi autoplay'i durdurmaz
-    // },
-    // speed: 800, // Geçiş hızını ayarla
+    effect: "creative", // Creative efekti kullanıyoruz
+    creativeEffect: {
+        prev: {
+            // Önceki slaytın kaybolma şekli
+            translate: ["-120%", 0, -500],
+            opacity: 0.5,
+        },
+        next: {
+            // Sonraki slaytın gelme şekli
+            translate: ["120%", 0, -500],
+            opacity: 0.5,
+        },
+    },
+    autoplay: {
+        delay: 3000, // Otomatik geçiş süresi (3 saniye)
+        disableOnInteraction: false, // Kullanıcı etkileşimi autoplay'i durdurmaz
+    },
+    speed: 800, // Geçiş hızını ayarla
 });
