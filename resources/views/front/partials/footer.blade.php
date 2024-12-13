bc508ecf692155f5a833294a35f4f3ac60459e74
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-12-14 01:43:38 +0300

    fix

--- resources/views/front/partials/footer.blade.php
+++ resources/views/front/partials/footer.blade.php
@@ -1,1 +1,7 @@
-<footer></footer>
+<footer class="bg-[#051e33] py-10">
+    <div class="container max-w-screen-xl mx-auto flex justify-center">
+        <div class="text-center text-white text-4xl">
+            Copyright © Jewelleryz
+        </div>
+    </div>
+</footer>
